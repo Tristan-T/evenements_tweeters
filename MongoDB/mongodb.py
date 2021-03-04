dce8aa7f9c4df7285c3172acce2fc80ad6b1f1ee
Tristan-T <tristan.tribes@etu.umontpellier.fr>
2021-03-04 11:19:59 +0100

    Added mongoDB proof of concept

--- MongoDB/mongodb.py
+++ MongoDB/mongodb.py
@@ -1,5 +1,6 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, GEO2D
 import urllib
+import datetime
 
 #TODO : Securely store password
 password = str(input("Please enter DB password : "))
@@ -7,6 +8,37 @@ password = str(input("Please enter DB password : "))
 passwordEncoded = urllib.parse.quote_plus(password)
 
 client = MongoClient("mongodb+srv://terL3:" + passwordEncoded + "@dbtweet.fakza.mongodb.net/DBTweet?retryWrites=true&w=majority")
-db=client.admin
+db=client.DBTweet
+tweetValide=db.valide
+tweetRealTime=db.real_time
+
 serverStatusResult=db.command("serverStatus")
-print(serverStatusResult)
+
+
+d = datetime.datetime.strptime("2021-02-07T16:44:53", "%Y-%m-%dT%H:%M:%S")
+
+listeTV = tweetValide.find()
+for tweet in listeTV:
+        print(tweet)
+
+    
+"""
+Cette fonction ajoute un tweet à la base de données "real-time" du programme.
+Elle prends en paramètre :
+        - L'ID du tweet, qui sera utilisé comme _id dans MongoDB
+        - Le string JSON du tweet
+        - La date de publication du tweet (convertissable en objet Date)
+        - La location GPS du tweet (longitude puis latitude)
+"""
+def addTweetDB(idTweet, jsonData, date, location):
+        d = datetime.datetime.strptime("2021-02-07T16:44:53", "%Y-%m-%dT%H:%M:%S")
+        
+        tweetRealtime.insert_one({
+            "_id": str(idTweet),
+            "json": jsonData,
+            "date": d,
+            "location": {"lng":-73.41 , "lat":40.764}
+        }) 
+        
+
+#print(serverStatusResult)
